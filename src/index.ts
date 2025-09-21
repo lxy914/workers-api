9f188a4eea9bc694962eca1013089ba80e154c49
lxy <314872695@qq.com>
2025-09-21 18:05:06 +0800

    0921

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,7 @@ const app = new Hono<{ Bindings: CloudflareBindings, Variables: Variables }>();
 
 app.use('/api/*', cors())
 app.use("/api/post/*", jwt_verify)
-app.get("/", (c) => {
-  return c.text("Hello!");
-});
+
 app.route("/api/user", user);
 app.route("/api/post", post);
 
