58cc05b410f02596744457b390894636f0baac77
lxy <314872695@qq.com>
2026-02-15 14:21:57 +0800

    优化sql

--- src/post.ts
+++ src/post.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { except } from "hono/combine";
 
 export const post = new Hono<{ Bindings: CloudflareBindings }>();
 
@@ -19,23 +20,20 @@ post.post('/', async (c) => {
     if (!param.title || !param.body) {
         return c.json({ code: 500, msg: 'title或body参数不能为空' })
     }
-    const count = await (await c.env.DB.exec(`insert into t_post (title, body, update_time) values ('${param.title}','${param.body}',${Date.now()})`)).count
-    if (count != 1) {
-        return c.json({ code: 500, msg: 'Can not create new post' })
+    try{
+        await c.env.DB.prepare(`insert into t_post (title, body, update_time) values (?,?,?)`).bind(param.title,param.body,Date.now()).run()
+        return c.json({ code: 200, msg: "操作成功" })
+    }catch(e){
+        return c.json({ code: 500, msg: e })
     }
-    return c.json({ code: 200, msg: "操作成功" })
+    
 })
 post.put('/:id', async (c) => {
     const id = c.req.param('id')
     const param = await c.req.json()
-    if (param.title && param.body) {
-        await c.env.DB.exec(`update t_post set title = '${param.title}', body = '${param.body}',update_time = ${Date.now()} where id=${id}`)
-        // return c.json({ code: 500, msg: 'title或body参数不能同时为空' })
+    if (!param.title || !param.body) {
+        return c.json({ code: 500, msg: 'title或body参数不能为空' })
     }
-    if (param.title){
-        await c.env.DB.exec(`update t_post set title = '${param.title}',update_time = ${Date.now()} where id=${id}`)
-    }else if (param.body) {
-        await c.env.DB.exec(`update t_post set body = '${param.body}',update_time = ${Date.now()} where id=${id}`)
-    }
+    await c.env.DB.prepare(`update t_post set title = ?,body = ? where id = ?`).bind(param.title,param.body,id).run()
     return c.json({ code: 200, msg: "操作成功" })
 })
